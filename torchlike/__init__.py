37e95fd3020bc2dc7a28d972067cbce6fb6bbc34
Fathur <rizkyfathur326@gmail.com>
2026-03-05 01:55:13 +0700

    feat(init): add __init__.py to initialize the torchlike module

---
+++ torchlike/__init__.py
@@ -0,0 +1,14 @@
+from . import functional
+from . import nn
+from . import optim
+from .data import ArrayDataset, DataLoader
+from .tensor import Tensor
+
+__all__ = [
+    "functional",
+    "nn",
+    "optim",
+    "ArrayDataset",
+    "DataLoader",
+    "Tensor",
+]
